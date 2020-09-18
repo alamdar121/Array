ce6a38e9f65617f17a4f7b3099ed174d4eab6d90
alamdar121 <alamdar.abass52@gmail.com>
2020-09-19 01:11:55 +0530

    Create sortedBinaryArray.java

---
+++ sortedBinaryArray.java
@@ -0,0 +1,26 @@
+Class Main
+{
+  public static void sortedBinaryArray(int arr[], int n)
+  {
+    int j=-1;
+    for(int i=0;i<n;i++)
+    {
+      if(a[i]>1)
+      {
+        j++;
+        int temp=a[j];
+        a[j]=a[i];
+        a[i]=temp;
+       }
+    }
+  }
+  public static void main(String args[])
+  {
+    int arr[]={0,1,0,1,1,0,0,1,1,1,0};
+    int n=arr.length;
+    sortedBinaryArray(arr,n);
+    for(int i=0;i<n;i++)
+    {
+      System.out.print(arr[i]+" ");
+    }
+}
